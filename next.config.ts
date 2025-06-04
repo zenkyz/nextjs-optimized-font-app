b1641d80023fa9b111100d1935b68a5b9f9737a7
zenkyz <epixgamer12@gmail.com>
2025-06-03 21:43:58 -0300

    Add next.config.ts 1748997806318

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
