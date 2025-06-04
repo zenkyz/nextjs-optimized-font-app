08d64163ec5e759d3365b9a25b0eb3e454aacf9d
zenkyz <epixgamer12@gmail.com>
2025-06-03 21:43:53 -0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1748997800846

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_0766a282._.js",
+  "static/chunks/src_app_page_tsx_b025fed5._.js"
+],
+    source: "dynamic"
+});
